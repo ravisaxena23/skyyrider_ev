8faff8568df3068defd60304db3de886fb081890
ravisaxena23 <saxenaravi23599@gmail.com>
2020-11-17 16:13:12 +0530

    pulled new changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,20 +1,28 @@
 import React, { Component } from "react";
-import Navbar from './navbar';
-import Footer from './footer';
+import Navbar from "./navbar";
+import Footer from "./footer";
 
 class home extends Component {
-    // state = {  }
-    render(){
-        const { set_num1,home}= this.props;
-        return(
-            <div>
-                <input type="text" name="num1" onChange={(e)=>{set_num1(e.target.value)}}/>
-                <input type="text" />
-                {home.num1}
-                <button >Submit</button>
-            </div>
-        )
-    }
+  // state = {  }
+  render() {
+    const { set_num1, home } = this.props;
+    return (
+      <div>
+        <Navbar />
+        <input
+          type="text"
+          name="num1"
+          onChange={(e) => {
+            set_num1(e.target.value);
+          }}
+        />
+        <input type="text" />
+        {home.num1}
+        <button>Submit</button>
+        <Footer />
+      </div>
+    );
+  }
 }
- 
+
 export default home;
