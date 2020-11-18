9d8ffc58fdf8f5d5d443fa158f8ba0ef4af5408b
ravisaxena23 <saxenaravi23599@gmail.com>
2020-11-18 23:53:41 +0530

    corrected navbar added homepage and aboutus page

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -11,43 +11,90 @@ import Avatar from "@material-ui/core/Avatar";
 import EmailIcon from "@material-ui/icons/Email";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import PhoneIcon from "@material-ui/icons/Phone";
+import Divider from "@material-ui/core/Divider";
 
 function Navbar(props) {
   return (
     <div style={{ color: "#fff" }}>
       <AppBar position="fixed">
         <Toolbar style={{ backgroundColor: "#fafafa" }}>
-          <Grid container spacing={3}>
-            <Grid item sm={2} md={1}>
-              <Hidden mdUp>
-                <Avatar alt="Remy Sharp" src={logo} />
-              </Hidden>
-              <Hidden smDown>
-                {" "}
-                <img src={logo} height={80} width={80}  alt="logo" />
-              </Hidden>
-            </Grid>
+          <Hidden smDown>
+            <Grid container spacing={3}>
+              <Grid item sm={2} md={1}>
+                <img src={logo} height={80} width={80} alt="logo" />
+              </Grid>
 
-            <Grid item sm={2} md={3}>
-              <Typography variant="body1" class="sub-head">
-                <AccessTimeIcon style={{ fontSize: "2vw", color:"#e5081f" }} />
-                12:00 22:00
-              </Typography>
-            </Grid>
+              <Divider orientation="vertical" flexItem />
 
-            <Grid item sm={2} md={5}>
-              <Typography variant="body1" class="sub-head">
-                {" "}
-                <EmailIcon style={{ fontSize: "2vw", color:"#e5081f" }} /> skyyriderev@gmail.com
-              </Typography>
-            </Grid>
+              <Grid item sm={2} md={3}>
+                <Typography variant="body1" class="sub-head">
+                  <AccessTimeIcon
+                    style={{ fontSize: "32px", color: "#e5081f" }}
+                  />
+                  12:00 22:00
+                </Typography>
+              </Grid>
 
-            <Grid item sm={2} md={3} class="nav-pad">
-              <Typography variant="body1" class="sub-head">
-                <PhoneIcon style={{ fontSize: "2vw", color:"#e5081f" }} /> +91 9999999999
-              </Typography>
+              <Divider orientation="vertical" flexItem />
+
+              <Grid item sm={2} md={5}>
+                <Typography variant="body1" class="sub-head">
+                  <EmailIcon style={{ fontSize: "32px", color: "#e5081f" }} />{" "}
+                  skyyriderev@gmail.com
+                </Typography>
+              </Grid>
+
+              <Divider orientation="vertical" flexItem />
+
+              <Grid item sm={2} md={3} class="nav-pad">
+                <Typography variant="body1" class="sub-head">
+                  <PhoneIcon style={{ fontSize: "32px", color: "#e5081f" }} />{" "}
+                  +91 9999999999
+                </Typography>
+              </Grid>
             </Grid>
-          </Grid>
+          </Hidden>
+
+          <Hidden mdUp>
+            <Grid container spacing={3}>
+              <Grid item sm={12} md={1} xs={12}>
+                <img
+                  src={logo}
+                  height={60}
+                  width={60}
+                  alt="logo"
+                  class="center-logo"
+                />
+              </Grid>
+
+              <Grid item sm={12} md={3} xs={12} className="px-0">
+                <Typography variant="body1" class="sub-head-mb">
+                  <AccessTimeIcon
+                    style={{ fontSize: "12px", color: "#e5081f" }}
+                  />
+                  12:00 22:00
+                </Typography>
+              </Grid>
+
+              <Divider />
+
+              <Grid item sm={12} md={5} xs={12} className="px-0">
+                <Typography variant="body1" class="sub-head-mb">
+                  <EmailIcon style={{ fontSize: "12px", color: "#e5081f" }} />{" "}
+                  skyyriderev@gmail.com
+                </Typography>
+              </Grid>
+
+              <Divider />
+
+              <Grid item sm={12} md={3} xs={12} className="px-0">
+                <Typography variant="body1" class="sub-head-mb">
+                  <PhoneIcon style={{ fontSize: "12px", color: "#e5081f" }} />{" "}
+                  +91 9999999999
+                </Typography>
+              </Grid>
+            </Grid>
+          </Hidden>
         </Toolbar>
 
         <Toolbar style={{ backgroundColor: "#0080ff" }}>
--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,24 +1,141 @@
-import React, { Component } from "react";
+import React from "react";
+import img1 from "../../img/3.jpeg";
 
-class home extends Component {
-  // state = {  }
-  render() {
-    const { set_num1, home } = this.props;
-    return (
-      <div>
-        <input
-          type="text"
-          name="num1"
-          onChange={(e) => {
-            set_num1(e.target.value);
-          }}
-        />
-        <input type="text" />
-        {home.num1}
-        <button>Submit</button>
-      </div>
-    );
-  }
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import Hidden from "@material-ui/core/Hidden";
+
+function Example(props) {
+  var items = [
+    {
+      name: "Random Name #1",
+      description: "Probably the most random thing you have ever seen!",
+      image: "https://ravi-portfolio.s3.us-east-2.amazonaws.com/4.jpeg",
+    },
+    {
+      name: "Random Name #2",
+      description: "Hello World!",
+      image: "https://ravi-portfolio.s3.us-east-2.amazonaws.com/6.jpeg",
+    },
+  ];
+
+  return (
+    <div>
+      <Carousel interval={5000}>
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
+      </Carousel>
+
+      <Hidden mdUp>
+        <Typography variant="h4" className="common-text home-head-3">
+          WELCOME TO SKYYRIDER EV
+        </Typography>
+      </Hidden>
+
+      <Hidden smDown>
+        <Typography variant="h4" className="common-text home-head-2">
+          WELCOME TO SKYYRIDER EV
+        </Typography>
+      </Hidden>
+
+      <Card className="home-card">
+        <Grid container spacing={4}>
+          <Grid
+            item
+            xs={12}
+            sm={4}
+            direction="row"
+            justify="flex-end"
+            className="home-grid"
+          >
+            <Typography
+              variant="h4"
+              className="common-text"
+              style={{ marginLeft: "10px" }}
+            >
+              We are qualified & experience in this field
+            </Typography>
+            <Typography variant="body1" className="common-text home-body-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+              sociosqu ad litora torquent per conubia nostra, per inceptos
+              himenaeos. Curabitur sodales ligula in libero. Sed dignissim
+              lacinia nunc.
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12} sm={8} className="home-grid">
+            <img src={img1} width="100%" height="auto" alt="placeholder"></img>
+          </Grid>
+        </Grid>
+      </Card>
+    </div>
+  );
 }
 
-export default home;
+function Item(props) {
+  return (
+    <Paper style={{ minHeight: "650px", maxHeight: "650px" }}>
+      <Hidden mdUp>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h4" className="common-text home-head">
+              SKYYRIDER
+            </Typography>
+            <Typography variant="h3" className="common-text home-subhead">
+              EV
+            </Typography>
+            <Button variant="outlined" color="primary" className="home-btn">
+              <Typography
+                variant="h4"
+                className="common-text home-mobilebtntxt"
+              >
+                EXPLORE
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img
+              src={props.item.image}
+              width="80%"
+              height="auto"
+              style={{ position: "absolute", top: "400px" }}
+            ></img>
+          </Grid>
+        </Grid>
+      </Hidden>
+
+      <Hidden smDown>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Typography variant="h2" className="common-text home-head">
+              SKYYRIDER
+            </Typography>
+            <Typography variant="h2" className="common-text home-subhead">
+              EV
+            </Typography>
+            <Button variant="outlined" color="primary" className="home-btn">
+              <Typography variant="h2" className="common-text home-btntxt">
+                EXPLORE
+              </Typography>
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <img src={props.item.image} width="80%" height="auto"></img>
+          </Grid>
+        </Grid>
+      </Hidden>
+    </Paper>
+  );
+}
+
+export default Example;
